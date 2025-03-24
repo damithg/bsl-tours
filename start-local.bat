dd6a8384d21c1a935605596ec1ea6591efb5830d
Damith Gunawardana <damithg@gmail.com>
2025-03-24 18:00:01 +0000

    Move files around

--- start-local.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting BSL Tours Website locally...
-set PORT=3000
-npm run dev
--- start-local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting BSL Tours Website locally..."
-export PORT=3000
-npm run dev
